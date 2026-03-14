061858ca56e826a3bdb2b223567d92b7006be111
jackan04 <jacob.asker@icloud.com>
2026-03-14 17:22:56 +0100

    feat: implement getConversations function

--- controllers/conversationsController.js
+++ controllers/conversationsController.js
@@ -1,5 +1,27 @@
-export const getConversations = async (req, res) => {
-  res.status(501).json({ message: "Not implemented" });
+import { prisma } from "../lib/prisma.js";
+
+export const getConversations = async (req, res, next) => {
+  try {
+    const conversations = await prisma.conversation.findMany({
+      where: {
+        participants: {
+          some: { id: req.user.id },
+        },
+      },
+      include: {
+        participants: {
+          id: true,
+          username: true,
+          displayName: true,
+        },
+        messages: true,
+      },
+    });
+
+    res.json(conversations);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const createConversation = async (req, res) => {
