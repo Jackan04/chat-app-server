e14cb26dbac2187c26e2cd666cd3c105c81daa78
jackan04 <jacob.asker@icloud.com>
2026-03-14 16:20:09 +0100

    feat: implement getUsers and getUserById functions in usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,30 @@
+import { prisma } from "../lib/prisma.js";
+
 export const getUsers = async (req, res) => {
-  res.status(501).json({ message: "Not implemented" });
+  try {
+    const users = await prisma.user.findMany();
+    res.json({ users });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getUserById = async (req, res) => {
-  res.status(501).json({ message: "Not implemented" });
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.json({ user });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateUser = async (req, res) => {
-  res.status(501).json({ message: "Not implemented" });
+  
 };
 
 export const deleteUser = async (req, res) => {
