cf1aee443243d792c183737b2e3915386e8fa770
jackan04 <jacob.asker@icloud.com>
2026-03-14 16:35:22 +0100

    feat: implement updateUser and deleteUser

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,7 @@ import { prisma } from "../lib/prisma.js";
 export const getUsers = async (req, res) => {
   try {
     const users = await prisma.user.findMany();
-    res.json({ users });
+    res.json(users);
   } catch (error) {
     next(error);
   }
@@ -17,16 +17,33 @@ export const getUserById = async (req, res) => {
       },
     });
 
-    res.json({ user });
+    res.json(user);
   } catch (error) {
     next(error);
   }
 };
 
 export const updateUser = async (req, res) => {
-  
+  const { displayName, bio, online } = req.body;
+
+  const user = await prisma.user.update({
+    where: { id: req.params.id },
+    data: {
+      displayName: displayName,
+      bio: bio,
+      online: online,
+    },
+  });
+
+  res.json(user);
 };
 
 export const deleteUser = async (req, res) => {
-  res.status(501).json({ message: "Not implemented" });
+  const user = await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.json(user);
 };
