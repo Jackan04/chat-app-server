a956ee46c65cacdadaef97035cac7bbd84e9caa5
jackan04 <jacob.asker@icloud.com>
2026-03-16 19:37:23 +0100

    feat: implement toggleUserStatus function; add new route

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,15 +2,17 @@ import { Router } from "express";
 import {
   getUsers,
   getUserById,
+  toggleUserStatus,
   updateUser,
   deleteUser,
 } from "../controllers/usersController.js";
 
 const usersRouter = Router();
 
-usersRouter.get("/",  getUsers);
+usersRouter.get("/", getUsers);
 usersRouter.get("/:id", getUserById);
 usersRouter.patch("/:id", updateUser);
+usersRouter.patch("/:id/status", toggleUserStatus);
 usersRouter.delete("/:id", deleteUser);
 
 export default usersRouter;
